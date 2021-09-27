59bead36470f89d90469ef615606bcf168fb3fd8
Can202 <mgoopazo@gmail.com>
2021-09-26 23:16:06 -0300

    Forum

--- init.py
+++ init.py
@@ -3,6 +3,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import os
 
+import webbrowser
+
 def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -10,6 +12,7 @@ def main():
     ui = mainDesign.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
+    ui.actionForum.triggered.connect(openForum)
     ui.actionAbout.triggered.connect(lambda: print("help"))
     ui.InstallButton.clicked.connect(install_package)
 
@@ -17,8 +20,14 @@ def main():
     MainWindow.setFixedSize(MainWindow.size())
     sys.exit(app.exec_())
 
+def openForum():
+    webbrowser.open("https://github.com/idur-package/Visual-Idur/discussions")
+
 def install_package():
     os.system("xterm -e 'read install && apt install $install && read'")
+def search_package():
+    pass
+
 
 
 if __name__ == "__main__":
