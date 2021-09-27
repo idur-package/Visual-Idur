bbc67b985d0034eec01fab891d75310dcc54e6c4
Can202 <mgoopazo@gmail.com>
2021-09-27 08:54:38 -0300

    compile

--- compile.sh
+++ compile.sh
@@ -5,6 +5,8 @@ cd $(dirname $0)
 if [ -f "/bin/python3" ] && [ -f "/bin/pip3" ]
 then
 	echo "Python3 and Pip3 installed"
+	rm -vrf export/*
+	mkdir -p export/
 	pip3 install pyinstaller pyqt5
 	pyinstaller --distpath export/ -F -n visual-idur src/init.py
 else
