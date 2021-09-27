1399072981806b9a6da366d2f7b69477f502e70d
Can202 <mgoopazo@gmail.com>
2021-09-26 22:59:33 -0300

    test

--- init.py
+++ init.py
@@ -1,13 +1,24 @@
 import design
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
+import os
 
-if __name__ == "__main__":
+def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = design.Ui_MainWindow()
     ui.setupUi(MainWindow)
+
+    ui.InstallButton.clicked.connect(install_package)
+
     MainWindow.show()
     MainWindow.setFixedSize(MainWindow.size())
     sys.exit(app.exec_())
+
+def install_package():
+    os.system("xterm -e 'read install && apt install $install && read'")
+
+
+if __name__ == "__main__":
+    main()
